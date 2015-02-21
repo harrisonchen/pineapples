f678f2d6ea3a59373bc396669494c16967c2d59e
Harrison Chen <harrisonxchen@gmail.com>
2015-02-21 05:24:53 -0800

    wip: pineapple service

--- pineapples.js
+++ pineapples.js
@@ -12,9 +12,29 @@ pineapples.directive('juiceBox', ['PineappleService', function(PineappleService)
 		},
 		template: "<h2>Results</h2>" +
 							"<div class='juice-box'>" +
+								"<ul>" +
+									"<li ng-repeat='pineapple in pineapples track by $index'>" +
+										"<h3>{{pineapple.title}}</h3>" +
+									"</li>" +
+								"</ul>" +
 							"</div>",
 		controller: function($scope, $element) {
-			PineappleService.getPineapples();
+			$scope.pineapples = [];
+
+			PineappleService.getPineapples()
+			.success(function(response) {
+				for(pineapple in response) {
+					var recipe = {};
+
+					recipe["image_url"] = pineapple.image_url;
+					recipe["title"] = pineapple.title;
+					recipe["source_url"] = pineapple.source_url;
+					recipe["social_rank"] = pineapple.social_rank;
+					recipe["publisher"] = pineapple.publisher;
+
+					$scope.pineapple.push(recipe);
+				}
+			});
 		},
 		link: function(scope, element, attrs) {
 
@@ -29,14 +49,17 @@ pineapples.factory('PineappleService', ['$http', '$q', function($http, $q) {
 
       var req = {
         method: 'POST',
-        url: 'http://food2fork.com/api/search?key=32f9c504981b7c8dc09c2bbd330a99b4&q=shredded%20chicken'
+        url: 'http://food2fork.com/api/search?key=32f9c504981b7c8dc09c2bbd330a99b4&q=shredded%20chicken',
+        headers: {
+        	'Access-Control-Allow-Origin': '*'
+        }
       };
 
       $http(req)
       .success(function(response) {
       	console.log(response);
 
-        deferred.resolve(data);
+        deferred.resolve(response);
 			});
 		}
 	};
