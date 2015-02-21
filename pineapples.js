eb3319e25b7877f83ac127edc8a45773b70265eb
Harrison Chen <harrisonxchen@gmail.com>
2015-02-21 08:04:46 -0800

    add awesome pinapple animations / transitions

--- pineapples.js
+++ pineapples.js
@@ -8,7 +8,7 @@ pineapples.directive('juiceBox', ['PineappleService', function(PineappleService)
 	return {
 		restrict: 'AE',
 		scope: {
-
+			searchTerm: '='
 		},
 		template: "<button class='pill-btn search-btn' ng-click='searchPineapples()'>Search!</button>" +
 							"<div data-ng-show='searched'>" +
@@ -16,8 +16,12 @@ pineapples.directive('juiceBox', ['PineappleService', function(PineappleService)
 								"<div class='juice-box'>" +
 									"<ul>" +
 										"<li ng-repeat='pineapple in pineapples track by $index'>" +
-											"<a ng-href='{{pineapple.source_url}}''><h3>{{pineapple.title}}</h3></a>" +
-											"<img ng-src='{{pineapple.image_url}}' />" +
+											"<h3>{{pineapple.title}}</h3>" +
+											"<div class='recipe-img'>" +
+												"<div class='overlay'></div>" +
+												"<div class='star'>{{pineapple.social_rank}} <i class='fa fa-star fa-2x'></i></div>" +
+												"<a ng-href='{{pineapple.source_url}}'><img ng-src='{{pineapple.image_url}}' /></a>" +
+											"</div>" +
 										"</li>" +
 									"</ul>" +
 								"</div>" +
@@ -27,8 +31,9 @@ pineapples.directive('juiceBox', ['PineappleService', function(PineappleService)
 			$scope.pineapples = [];
 
 			$scope.searchPineapples = function() {
-				PineappleService.getPineapples()
+				PineappleService.getPineapples($scope.searchTerm)
 				.then(function(response) {
+					console.log($scope.searchTerm);
 					$scope.searched = true;
 					$scope.pineapples = [];
 					var recipes = response.recipes;
@@ -39,7 +44,7 @@ pineapples.directive('juiceBox', ['PineappleService', function(PineappleService)
 						recipe["image_url"] = recipes[i].image_url;
 						recipe["title"] = recipes[i].title;
 						recipe["source_url"] = recipes[i].source_url;
-						recipe["social_rank"] = recipes[i].social_rank;
+						recipe["social_rank"] = Math.ceil(recipes[i].social_rank);
 						recipe["publisher"] = recipes[i].publisher;
 
 						$scope.pineapples.push(recipe);
@@ -56,12 +61,12 @@ pineapples.directive('juiceBox', ['PineappleService', function(PineappleService)
 
 pineapples.factory('PineappleService', ['$http', '$q', function($http, $q) {
 	return {
-		getPineapples: function() {
+		getPineapples: function(query) {
 			var deferred = $q.defer();
 
       var req = {
         method: 'POST',
-        url: 'http://food2fork.com/api/search?key=32f9c504981b7c8dc09c2bbd330a99b4&q=shredded%20chicken',
+        url: 'http://food2fork.com/api/search?key=32f9c504981b7c8dc09c2bbd330a99b4&q=' + query,
         headers: {
         	'Access-Control-Allow-Origin': '*'
         }
