2c092d42b841dc5dd2f3cc8f542dc8a9ee320684
Harrison Chen <harrisonxchen@gmail.com>
2015-02-21 04:53:57 -0800

    setup pineapple service

--- pineapples.js
+++ pineapples.js
@@ -4,7 +4,7 @@ pineapples.controller('JuiceCtrl', function($scope) {
 	$scope.juice = "PINEAPPLES!!!!";
 });
 
-pineapples.directive('juiceBox', [function() {
+pineapples.directive('juiceBox', ['PineappleService', function(PineappleService) {
 	return {
 		restrict: 'AE',
 		scope: {
@@ -14,10 +14,30 @@ pineapples.directive('juiceBox', [function() {
 							"<div class='juice-box'>" +
 							"</div>",
 		controller: function($scope, $element) {
-
+			PineappleService.getPineapples();
 		},
 		link: function(scope, element, attrs) {
 
+		}
+	};
+}]);
+
+pineapples.factory('PineappleService', ['$http', '$q', function($http, $q) {
+	return {
+		getPineapples: function() {
+			var deferred = $q.defer();
+
+      var req = {
+        method: 'POST',
+        url: 'http://food2fork.com/api/search?key=32f9c504981b7c8dc09c2bbd330a99b4&q=shredded%20chicken'
+      };
+
+      $http(req)
+      .success(function(response) {
+      	console.log(response);
+
+        deferred.resolve(data);
+			});
 		}
 	};
 }]);
